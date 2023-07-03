29cb2e9bc7c615574482d7beb45f014e122439ca
VLuca997 <kulertoast997@gmail.com>
2023-07-03 02:38:58 +0200

    Add img

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
 // STAMPA "HELLO" TRAMITE VUE.JS
 
 
-const {createApp} = Vue 
+const { createApp } = Vue;
+
 const app = createApp({
-    data(){
-        return{
-            message: 'Hello Vue!'
-        }
+    data() {
+        return {
+        message: 'Hello Spidy!',
+        imageUrl: '/img/spidy.jpg'
+        };
     }
 });
+
 app.mount('#print-Message');
